035761ca9f953cf28f7bba7a2193004cd9be7491
NandiniGohel <ngohel2001@gmail.com>
2026-08-13 01:41:09 +0530

    update init file...

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-# This module ships no Python models/controllers.
-# The file must still exist (even empty) so Odoo's module loader
-# recognizes this directory as a valid Python package.
+from . import controllers
+from . import models
+from . import wizard
